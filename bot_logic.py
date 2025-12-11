304fa40b263c61dfcbf728a46e0f0492e1b0332d
Ana-Carolina51 <anacarolmallet15@gmail.com>
2025-12-11 16:39:02 +0000

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,8 @@
+import random
+
+def pass_gen(size):
+    caracteres = ("+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+    senha = ""
+    for _ in range(size):
+        senha += random.choice(caracteres)
+    return senha
